e6c3f5e43fd58ae78ead57c7b8c32e6aa3ab21e5
BugHunter <76444458+BugHunter021@users.noreply.github.com>
2024-10-10 13:11:36 +0330

    Update Recon-fuzz-dir.sh

--- modules/Custom/Recon-fuzz-dir.sh
+++ modules/Custom/Recon-fuzz-dir.sh
@@ -23,31 +23,31 @@ EOF
 
 domain_name=`echo $1 | cut -d / -f3`
 echo "Start ffuf for fuzz cve dirs with: fuzz-dir-cve.txt file"
-ffuf -u $1/FUZZ -w ./wordlist/fuzz-dir-cve.txt -H "User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; MSBrowserIE; rv:11.0) like Gecko" -H "X-Forwarded-For: 127.0.0.1" -fc 404 -of csv -t 1 -o $domain_name.fuzz-dir-cve.csv
+ffuf -u $1/FUZZ -w ./wordlist/fuzz-dir-cve.txt -H "User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; MSBrowserIE; rv:11.0) like Gecko" -H "X-Forwarded-For: 127.0.0.1" -fc 400,401,403,404 -of csv -or true -t 1 -o $domain_name.fuzz-dir-cve.csv
 echo "fuzz cve dirs Done & final result in $domain_name.fuzz-dir-cve.csv ==> len: ` cat $domain_name.fuzz-dir-cve.csv | wc -l `"
 
 sleep 5
 echo
 echo "Start ffuf for fuzz leaky-misconfigs dirs with: fuzz-dir-leaky-misconfigs.txt file"
-ffuf -u $1/FUZZ -w ./wordlist/fuzz-dir-leaky-misconfigs.txt -H "User-Agent: Mozilla/5.0 (Windows NT 5.1; rv:38.0) Gecko/20100101 Firefox/38.0" -H "X-Forwarded-For: 127.0.0.1" -fc 404 -of csv -t 1 -o $domain_name.fuzz-dir-leaky-misconfigs.csv
+ffuf -u $1/FUZZ -w ./wordlist/fuzz-dir-leaky-misconfigs.txt -H "User-Agent: Mozilla/5.0 (Windows NT 5.1; rv:38.0) Gecko/20100101 Firefox/38.0" -H "X-Forwarded-For: 127.0.0.1" -fc 400,401,403,404 -of csv -or true -t 1 -o $domain_name.fuzz-dir-leaky-misconfigs.csv
 echo "fuzz leaky-misconfigs dirs Done & final result in $domain_name.fuzz-dir-leaky-misconfigs.csv ==> len: ` cat $domain_name.fuzz-dir-leaky-misconfigs.csv | wc -l `"
 
 sleep 5
 echo
 echo "Start ffuf for fuzz backup_files dirs with: fuzz-dir-backup_files.txt file"
-ffuf -u $1/FUZZ -w ./wordlist/fuzz-dir-backup_files.txt -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:39.0) Gecko/20100101 Firefox/39.0" -H "X-Forwarded-For: 127.0.0.1" -fc 404 -of csv -t 1 -o $domain_name.fuzz-dir-backup_files.txt
+ffuf -u $1/FUZZ -w ./wordlist/fuzz-dir-backup_files.txt -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:39.0) Gecko/20100101 Firefox/39.0" -H "X-Forwarded-For: 127.0.0.1" -fc 400,401,403,404 -of csv -or true -t 1 -o $domain_name.fuzz-dir-backup_files.txt
 echo "fuzz backup_files dirs Done & final result in $domain_name.fuzz-dir-backup_files.txt ==> len: ` cat $domain_name.fuzz-dir-backup_files.txt | wc -l `"
 
 sleep 5
 echo
 echo "Start ffuf for fuzz backup_files & dirs with: fuzz-dir-api-info.txt file"
-ffuf -u $1/FUZZ -w ./wordlist/fuzz-dir-api-info.txt -H "User-Agent: Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET4.0C; .NET4.0E)" -H "X-Forwarded-For: 127.0.0.1" -fc 404 -of csv -t 1 -o $domain_name.fuzz-dir-api-info.csv
+ffuf -u $1/FUZZ -w ./wordlist/fuzz-dir-api-info.txt -H "User-Agent: Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET4.0C; .NET4.0E)" -H "X-Forwarded-For: 127.0.0.1" -fc 400,401,403,404 -of csv -t 1 -o $domain_name.fuzz-dir-api-info.csv
 echo "fuzz backup_files & dirs Done & final result in $domain_name.fuzz-dir-api-info.csv ==> len: ` cat $domain_name.fuzz-dir-api-info.csv | wc -l `"
 
 sleep 5
 echo
 echo "Start ffuf for fuzz cgi dirs with: fuzz-cgi-files.txt file"
-ffuf -u $1/FUZZ -w ./wordlist/fuzz-cgi-files.txt -H "User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64; Trident/7.0; rv:11.0) like Gecko" -H "X-Forwarded-For: 127.0.0.1" -fc 404 -of csv -t 1 -o $domain_name.fuzz-dir-cgi-files.csv
+ffuf -u $1/FUZZ -w ./wordlist/fuzz-cgi-files.txt -H "User-Agent: Mozilla/5.0 (Windows NT 6.1; Win64; x64; Trident/7.0; rv:11.0) like Gecko" -H "X-Forwarded-For: 127.0.0.1" -fc 400,401,403,404 -of csv -or true -t 1 -o $domain_name.fuzz-dir-cgi-files.csv
 echo "fuzz cgi dirs Done & final result in $domain_name.fuzz-dir-cgi-files.csv ==> len: ` cat $domain_name.fuzz-dir-cgi-files.csv | wc -l `"
 
 cat $domain_name.fuzz-dir-cve.csv $domain_name.fuzz-dir-leaky-misconfigs.csv $domain_name.fuzz-dir-backup_files.txt $domain_name.fuzz-dir-api-info.csv $domain_name.fuzz-dir-cgi-files.csv > $domain_name-fuzz-dir.csv
